3e612d5eb298c625176162eda9890a77a79a346c
Tselot Million <tselotmillion8@gmail.com>
2025-11-02 15:11:17 +0300

    low-level caching

--- properties/utils.py
+++ properties/utils.py
@@ -4,6 +4,6 @@ from .models import Property
 def get_all_properties():
     properties = cache.get('all_properties')
     if not properties:
-        properties = list(Property.objects.values())
-        cache.set('all_properties', properties, 3600)  # cache for 1 hour
+        properties = Property.objects.all()
+        cache.set('all_properties', properties, 3600)
     return properties
